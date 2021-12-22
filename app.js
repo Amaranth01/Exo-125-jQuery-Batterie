59841a344eda9af01fa9bbbaf4d59417c7425aea
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-22 12:49:09 +0100

    finit

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-function sound (e) {
-    //recuperation elements key and audio
-    let keyElement = $(`.key[data-key="${e.keyCode}"]`);
-    let audio = $(`.sound[data-key="${e.keyCode}"]`);
+window.addEventListener('keydown', function (e) {
+    switch (e.keyCode) {
+        case 65 :
+            $('audio[data-key="65"]')[0].play();
+            break;
 
-    //recuperation of class playing
-    $('.key').addClass('playing');
-    audio.play();
-}
+        case 83 :
+            $('audio[data-key="83"]')[0].play();
+            break;
 
-sound();
+        case 68 :
+            $('audio[data-key="68"]')[0].play();
+            break;
+
+        case 70 :
+            $('audio[data-key="70"]')[0].play();
+            break;
+
+        case 71 :
+            $('audio[data-key="71"]')[0].play();
+            break;
+
+        case 72 :
+            $('audio[data-key="72"]')[0].play();
+            break;
+
+        case 74 :
+            $('audio[data-key="74"]')[0].play();
+            break;
+
+        case 75 :
+            $('audio[data-key="75"]')[0].play();
+            break;
+
+        case 76 :
+            $('audio[data-key="76"]')[0].play();
+            break;
+    }
+});
+
+$('.key').addClass('playing');
